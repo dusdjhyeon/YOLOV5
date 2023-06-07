9f3f77d40f558222730211a62754d10430226c9c
dusdjhyeon <dusdj0813@gmail.com>
2023-06-07 14:19:27 +0900

    yolov5

---
+++ test_api/test_api.py
@@ -0,0 +1,26 @@
+import requests
+
+HOST = 'http://127.0.0.1:8000'
+res = requests.post(HOST + '/api-token-auth/', {
+    'username':'dusdj',
+    'password':'rlatjdrb0428',
+})
+
+res.raise_for_status()
+token = res.json()['token']
+print(token)
+
+# 인증이 필요한 요청에 아래의 headers를 붙임
+headers = {'Authorization': 'JWT' + token, 'Accept': 'application/json'}
+
+# Post Create
+data = {
+    'title' : '제목 by code',
+    'text' : 'API내용 by code',
+    'created_date' : '2023-06-01T10:00:00+09:00',
+    'published_date': '2023-06-01T10:00:00+09:00'
+}
+file = {'image': open('C:/Users/dusdj/project/star.jpg','rb')}
+res = requests.post(HOST + '/api_root/Post/', data=data, files=file, headers=headers)
+print(res)
+print(res.json())
